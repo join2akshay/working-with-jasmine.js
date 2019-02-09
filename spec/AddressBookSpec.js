c5961ff4bcb1e6f2b1f75ca61a4b0dc4d107e76a
tvibhu12 <tvibhu12@gmail.com>
2019-02-09 23:31:27 +0530

    try beforeEach function

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -1,13 +1,20 @@
 describe("AddressBook",function(){
+    var addressBook,
+    thisContact;
+    beforeEach(function(){
+        addressBook= new AddressBook();
+        thisContact = new Contact();
+
+    });
+
     it("It should be able to add contact",function(){
-        var addressBook= new AddressBook();
-         thisContact = new Contact();
+      
         addressBook.addContact(thisContact);
         expect(addressBook.getContact(0)).toBe(thisContact);
     });
     it("It should able to delete contact", function(){
-        var addressBook= new AddressBook();
-         thisContact = new Contact();
+      //  var addressBook= new AddressBook();
+      //   thisContact = new Contact();
         addressBook.addContact(thisContact);
         addressBook.deleteContact(0);
         expect(addressBook.getContact(0)).not.toBeDefined();
